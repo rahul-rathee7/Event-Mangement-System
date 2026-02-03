5b963ccc5a9964aa0ca9ca07e11f2771d18cb241
Rahul Rathee <ratheerahul602@gmail.com>
2026-02-03 20:23:25 +0530

    fix: samesite edited with none

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -108,7 +108,7 @@ export const registerUser = async (req, res) => {
 export const logoutUser = (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
     secure: true
   })
   res.status(200).json({ success: true, message: 'Logged out successfully' })
